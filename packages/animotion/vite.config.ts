d75d5159ec0099a25da968367e42c4670bd2ca11
reidpat <61036489+reidpat@users.noreply.github.com>
2024-10-15 15:35:14 -0700

    Slight Style Changes

--- packages/animotion/vite.config.ts
+++ packages/animotion/vite.config.ts
@@ -4,12 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 import svelteInspector from './svelte-inspector';
 
 export default defineConfig({
-	plugins: [svelteInspector(), sveltekit(), tailwindcss()]
-})
-
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface window {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	deck: any; // Adjust the type as needed
-  }
+  plugins: [svelteInspector(),
+  sveltekit(),
+  tailwindcss(),
+  ]
+});
