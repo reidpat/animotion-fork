4059816b8e882c81436ddd000de8aa4d3aeaad36
reidpat <61036489+reidpat@users.noreply.github.com>
2024-10-16 17:28:03 -0700

    Adding in daisyUI as cdn Removing vite-inspector

--- packages/animotion/svelte.config.js
+++ packages/animotion/svelte.config.js
@@ -7,17 +7,6 @@ const config = {
 	kit: {
 		adapter: adapter()
 	},
-	vitePlugin: {
-		// set to true for defaults or customize with object
-		inspector: {
-			toggleKeyCombo: 'control-shift',
-			showToggleButton: 'always',
-			toggleButtonPos: 'bottom-right'
-		}
-	},
-	server: {
-		HMR: { overlay: false }
-	}
 }
 
 export default config
